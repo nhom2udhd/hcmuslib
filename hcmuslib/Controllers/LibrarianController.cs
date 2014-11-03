089f3ee15a99b4a277302b4c4b18f4813758e6e8
Nhan Vo <nhan.vo@opendigital.com.au>
2014-11-03 20:51:39 +0700

    +Add controller Librarian +Add 3 views in Librarian

---
+++ hcmuslib/Controllers/LibrarianController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hcmuslib.Controllers
+{
+    public class LibrarianController : Controller
+    {
+        //
+        // GET: /Librarian/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ManageReaderImage()
+        {
+            return View();
+        }
+        public ActionResult BarcodeReader()
+        {
+            return View();
+        }
+        public ActionResult Analytics()
+        {
+            return View();
+        }
+    }
+}
