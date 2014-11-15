3910d769262ab546dda1c4f7f96472e52157e52b
Nhan Vo <nhan.vo@opendigital.com.au>
2014-11-15 18:43:05 +0700

    Create layout for Reader Index and Register Training

---
+++ hcmuslib/Controllers/ReaderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hcmuslib.Models;
+
+namespace hcmuslib.Controllers
+{
+    public class ReaderController : Controller
+    {
+        //
+        // GET: /Reader/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterTraining() 
+        {
+            return View();
+        }
+    }
+}
