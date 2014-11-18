2b7633e5e62571b29e54a51bc21e126974537063
luanngu0111 <luanngu.0111@gmail.com>
2014-11-18 16:43:18 +0700

    * Cap nhat chuc nang cho muon sach

--- hcmuslib/Controllers/CirculatorController.cs
+++ hcmuslib/Controllers/CirculatorController.cs
@@ -35,18 +35,40 @@ namespace hcmuslib.Controllers
             return View();
         }
 
+        public JsonResult GetInfo(string dg, string sach)
+        {
+            string[] rs = new string[2];
+            var docgia = (from d in data.DOCGIA where d.MS_THE == dg select d).First();
+            rs[0] = docgia.HO_TEN;
+            var sh=(from s in data.SACH where s.ID_SACH==sach select s).First();
+
+            string ttsach = sh.BMNHANDECHINH.NHAN_DE_CHINH + "; " + sh.BMTACGIA.BUT_DANH + "; " + sh.BMNXB.TEN_NXB;
+            rs[1] = ttsach;
+            return Json(new {docgia= rs[0], sach=rs[1]});
+        }
+
         [HttpPost]
         public ActionResult SaveLentBookInfo(FormCollection f)
         {
-            
-            int size = f.Count;
-            Random ran = new Random();
+
+            int size = f.GetValues("docgia[]").Length ;
+            Random ran = new Random(12);
+            string str = ran.Next(0, 99999999).ToString();
+            string id = "LH"+str.PadLeft(8, '0');
+            var obj = (from p in data.LUUHANHSACH where p.ID_LUU_HANH == id select p).ToList();
+            while (obj.Count != 0)
+            {
+                str = ran.Next(0, 99999999).ToString();
+                id = "LH" + str.PadLeft(8, '0');
+                obj = (from p in data.LUUHANHSACH where p.ID_LUU_HANH == id select p).ToList();
+            }
+
             for (int i = 0; i < size; i++)
             {
                 LUUHANHSACH luuhanh = new LUUHANHSACH();
                 luuhanh.DOC_GIA = f.GetValues("docgia[]")[i];
                 luuhanh.ID_SACH = f.GetValues("sach[]")[i];
-                luuhanh.ID_LUU_HANH = "LH" + ran.Next().ToString();
+                luuhanh.ID_LUU_HANH = id;
                 luuhanh.NGAY_MUON = DateTime.Now;
                 luuhanh.THOI_HAN_MUON = DateTime.Now.AddDays(14);
                 data.LUUHANHSACH.Add(luuhanh);
